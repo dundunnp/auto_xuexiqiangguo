1031f8788686c15e94488d2984e87b90b7400dad
dundun <68000706+dundunnp@users.noreply.github.com>
2022-05-20 09:37:53 +0800

    Update 学习强国.js

--- Hamibot/学习强国.js
+++ Hamibot/学习强国.js
@@ -1029,6 +1029,32 @@ function do_periodic_answer(number) {
     }
 }
 
+/**
+ * 处理访问异常
+ */
+function handling_access_exceptions() {
+    if (text("访问异常").exists()) {
+        // 滑动按钮位置
+        var pos = className('android.view.View').depth(10).clickable(true).findOnce(1).bounds();
+        // 滑动框右边界
+        var right_border = className('android.view.View').depth(9).clickable(false).findOnce(0).bounds().right;
+        // 位置取随机值
+        var randomX = random(pos.left, pos.right);
+        var randomY = random(pos.top, pos.bottom);
+        swipe(randomX, randomY, randomX + right_border, randomY, random(200, 400));
+    }
+}
+
+/* 
+处理访问异常，滑动验证
+*/
+var id_handling_access_exceptions;
+// 在子线程执行的定时器，如果不用子线程，则无法获取弹出页面的控件
+var thread_handling_access_exceptions = threads.start(function () {
+    // 每2秒就处理访问异常
+    id_handling_access_exceptions = setInterval(handling_access_exceptions, 2000);
+});
+
 /*
  **********每日答题*********
  */
@@ -1184,32 +1210,6 @@ if (!finish_list[5] && special_answer_scored < 8) {
         className("android.view.View").clickable(true).depth(23).findOne().click();
     }
 }
-
-/**
- * 处理访问异常
- */
-function handling_access_exceptions() {
-    if (text("访问异常").exists()) {
-        // 滑动按钮位置
-        var pos = className('android.view.View').depth(10).clickable(true).findOnce(1).bounds();
-        // 滑动框右边界
-        var right_border = className('android.view.View').depth(9).clickable(false).findOnce(0).bounds().right;
-        // 位置取随机值
-        var randomX = random(pos.left, pos.right);
-        var randomY = random(pos.top, pos.bottom);
-        swipe(randomX, randomY, randomX + right_border, randomY, random(200, 400));
-    }
-}
-
-/* 
-处理访问异常，滑动验证
-*/
-var id_handling_access_exceptions;
-// 在子线程执行的定时器，如果不用子线程，则无法获取弹出页面的控件
-var thread_handling_access_exceptions = threads.start(function () {
-    // 每2秒就处理访问异常
-    id_handling_access_exceptions = setInterval(handling_access_exceptions, 2000);
-});
 
 /*
  **********挑战答题*********
