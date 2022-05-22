b34364d33b4a61e9ce9bd76369e7b60469ef58c2
dundun <68000706+dundunnp@users.noreply.github.com>
2022-05-22 09:08:24 +0800

    Update 学习强国.js

--- Hamibot/学习强国.js
+++ Hamibot/学习强国.js
@@ -130,7 +130,7 @@ if (!storage.contains("answer_question_map1")) {
     toast("正在下载题库");
     // 使用 Github 文件加速服务：https://git.yumenaka.net
     var answer_question_bank = http.get("https://git.yumenaka.net/https://raw.githubusercontent.com/Mondayfirst/XXQG_TiKu/main/%E9%A2%98%E5%BA%93_%E6%8E%92%E5%BA%8F%E7%89%88.json");
-    // 如果资源过期换成别的云盘
+    // 如果资源过期或无法访问则换成别的云盘
     if (!(answer_question_bank.statusCode >= 200 && answer_question_bank.statusCode < 300)) {
         // 使用腾讯云
         var answer_question_bank = http.get("https://xxqg-tiku-1305531293.cos.ap-nanjing.myqcloud.com/%E9%A2%98%E5%BA%93_%E6%8E%92%E5%BA%8F%E7%89%88.json");
@@ -1406,7 +1406,7 @@ if (!finish_list[8] && two_players_scored < 1) {
 }
 
 // 取消访问异常处理循环
-clearInterval(id_handling_access_exceptions);
+if (id_handling_access_exceptions) clearInterval(id_handling_access_exceptions);
 
 /*
  **********订阅*********
