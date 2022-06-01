b7751906105988fe615ad86bfc98848f53c187e4
dundun <68000706+dundunnp@users.noreply.github.com>
2022-06-01 10:11:56 +0800

    Update 学习强国.js

--- Auto.js/学习强国.js
+++ Auto.js/学习强国.js
@@ -918,17 +918,26 @@ function do_periodic_answer(number) {
 function handling_access_exceptions() {
     if (text("访问异常").exists()) {
         // 滑动按钮位置
+        className('android.view.View').depth(10).clickable(true).waitFor();
         var pos = className('android.view.View').depth(10).clickable(true).findOnce(1).bounds();
         // 滑动框右边界
+        className('android.view.View').depth(9).clickable(false).waitFor();
         var right_border = className('android.view.View').depth(9).clickable(false).findOnce(0).bounds().right;
-        // 位置取随机值
-        var randomX = random(pos.left, pos.right);
-        var randomY = random(pos.top, pos.bottom);
-        swipe(randomX, randomY, randomX + right_border, randomY, random(200, 400));
-        longClick(randomX + right_border, randomY);
-        if (textContains("刷新").exists()) {
-            click('刷新');
+        while (text("访问异常").exists() || text("刷新").exists()) {
+            // 位置取随机值
+            var randomX = random(pos.left, pos.right);
+            var randomY = random(pos.top, pos.bottom);
+            swipe(randomX, randomY, randomX + right_border, randomY, random(200, 400));
+            press(randomX + right_border, randomY, 600);
+            // 需要开启新线程获取控件
+            threads.start(function () {
+                if (text("刷新").exists()) {
+                    click('刷新');
+                }
+            });
         }
+        // 执行脚本只需通过一次验证即可，通过后将定时器关闭
+        threads.shutDownAll();
     }
     if (textContains("网络开小差").exists()) {
         click('确定');
